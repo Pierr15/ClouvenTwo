974e2c48a24e922cdaaab46e6e3a7a36df0dc71b
Rifqi Faizal <rifqicode23@gmail.com>
2026-08-29 17:08:45 +0700

    initial commit: CLOEV web

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  devIndicators: false,
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "nsjifawcftqwyseyfett.supabase.co",
+        pathname: "/storage/v1/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
